393f5a12d9ddd2fe00a30377571dee0e6049d5d7
Nishant Luthra <nishant.luthra30@gmail.com>
2022-11-16 23:38:47 +0530

    updated Student class

--- src/Student.java
+++ src/Student.java
@@ -2,20 +2,51 @@ import java.util.*;
 
 public class Student extends User {
 
-    private String id;
-    private String name;
-    private String branch;
-    private double cgpa;
-    private ArrayList<String> subjectsCompleted;
+    private final String id;
+    private final String name;
+    private final String branch;
+    private final double cgpa;
+    private final ArrayList<String> subjectsCompleted;
     private ArrayList<PS_Station> preferences;
 
-    public void setDetails() {
+    public Student(String id, String name, String branch, double cgpa, ArrayList<String> subjectsCompleted) {
+        this.id = id;
+        this.name = name;
+        this.branch = branch;
+        this.cgpa = cgpa;
+        this.subjectsCompleted = subjectsCompleted;
     }
 
-    public void getPSStationsDetails() {
+    public String getId() {
+        return id;
     }
 
-    public void submitPreferences() {
+    public String getName() {
+        return name;
+    }
+
+    public double getCgpa() {
+        return this.cgpa;
+    }
+
+    public String getBranch() {
+        return this.branch;
+    }
+
+    public ArrayList<PS_Station> getPreferences() {
+        return this.preferences;
+    }
+
+    public ArrayList<String> getSubjectsCompleted() {
+        return this.subjectsCompleted;
+    }
+
+    public ArrayList<PS_Station> getPSStationsDetails() { // TODO: COMPLETE
+        return null;
+    }
+
+    public void submitPreferences(ArrayList<PS_Station> preferences) { // TODO: COMPLETE
+        this.preferences = preferences;
     }
 
     public void acceptAllotment() {
