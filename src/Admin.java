22713844b329abb35796bb8ea0a865ac5dd5ec08
ishikakadam <88560216+ishikakadam@users.noreply.github.com>
2022-12-04 16:24:44 +0530

    Update Admin.java

--- src/Admin.java
+++ src/Admin.java
@@ -41,14 +41,13 @@ public class Admin extends User implements AdminActions, Runnable {
             studentList.offer(student);
         }
 
-        public void updatePSStationDetails (PS_Station updatedStation){
-            synchronized (update) {
+        public synchronized void updatePSStationDetails (PS_Station updatedStation){
+        
                 for (int i = 0; i < PS_StationsList.size(); i++) {
                     if (updatedStation.getName().equals(PS_StationsList.get(i).getName())) {
                         PS_StationsList.set(i, updatedStation);
                     }
-                }
-            }
+                }      
         }
 
         public void addPS_Station (PS_Station station){ // TODO: Input from file handling
